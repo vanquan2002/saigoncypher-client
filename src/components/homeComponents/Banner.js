797224fe23a10f8f7fb5a7365a65d5d0a6ffb99e
Pham Van Quan <quan100202@gmail.com>
2024-10-11 18:08:02 +0700

    update

--- src/components/homeComponents/Banner.js
+++ src/components/homeComponents/Banner.js
@@ -2,19 +2,30 @@ import React from "react";
 import { TbNorthStar } from "react-icons/tb";
 
 const Banner = () => {
+  // Đơn giản nhưng chẳng đơn điệu.
+  // Định hình phong cách riêng.
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-24">
-      <TbNorthStar className="text-6xl md:text-8xl lg:text-8xl" />
-      <h1 className="flex flex-col items-center font-black text-4xl md:text-7xl lg:text-8xl uppercase">
-        Saigoncypher
-        <span className="text-xs md:text-base font-medium">
-          Đơn giản nhưng chẳng đơn điệu
+    <div className="h-screen flex flex-col items-center justify-center gap-20">
+      <TbNorthStar className="text-6xl md:hidden" />
+      <h1 className="flex flex-col gap-3">
+        <span className="tracking-wider text-[18vw] md:text-[17vw] lg:text-[11vw] leading-none text-left font-black flex items-center justify-start">
+          Saig
+          <TbNorthStar className="hidden md:block" />
+          <span className="block md:hidden">o</span>n
         </span>
-        <span className="text-xs md:text-base font-medium">
-          Định hình phong cách riêng
+
+        <span className="tracking-wider text-[18vw] md:text-[17vw] lg:text-[11vw] leading-none text-center font-black">
+          Cypher
+        </span>
+
+        <span className="tracking-wider text-[18vw] md:text-[17vw] lg:text-[11vw] leading-none text-right font-black flex items-center justify-end">
+          St
+          <TbNorthStar className="hidden md:block" />
+          <span className="block md:hidden">o</span>
+          re
         </span>
       </h1>
-      <TbNorthStar className="text-6xl md:text-8xl lg:text-8xl" />
+      <TbNorthStar className="text-6xl md:hidden" />
     </div>
   );
 };
